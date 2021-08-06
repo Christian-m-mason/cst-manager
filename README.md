af74ff5af79d8c3d1b7e9eaecf247751fb5167df
Christian Mason <christian@cmason.io>
2021-08-05 22:28:37 -0400

    Refactored Sign-In Logic and added JWT

--- README.md
+++ README.md
@@ -2,20 +2,20 @@
 
 ## Tables
 
-| Schema | Name                    | Type     | Owner    |
-| ------ | ----------------------- | -------- | -------- |
-| public | branch                  | table    | postgres |
-| public | branches_branch_id_seq  | sequence | postgres |
-| public | position                | table    | postgres |
-| public | positions_id_seq        | sequence | postgres |
-| public | profile                 | table    | postgres |
-| public | rank                    | table    | postgres |
-| public | ranks_id_seq            | sequence | postgres |
-| public | response_sectors        | table    | postgres |
-| public | response_sectors_id_seq | sequence | postgres |
-| public | section                 | table    | postgres |
-| public | soldier_profiles_id_seq | sequence | postgres |
-| public | state                   | table    | postgres |
-| public | states_id_seq           | sequence | postgres |
-| public | team                    | table    | postgres |
-| public | users                   | table    | postgres |
+| Schema | Name                   | Type     | Owner    |
+| ------ | ---------------------- | -------- | -------- |
+| public | branch                 | table    | postgres |
+| public | branch_id_seq          | sequence | postgres |
+| public | position               | table    | postgres |
+| public | position_id_seq        | sequence | postgres |
+| public | profile                | table    | postgres |
+| public | profile_id_sequence    | sequence | postgres |
+| public | rank                   | table    | postgres |
+| public | rank_id_seq            | sequence | postgres |
+| public | response_sector        | table    | postgres |
+| public | response_sector_id_seq | sequence | postgres |
+| public | section                | table    | postgres |
+| public | state                  | table    | postgres |
+| public | state_id_seq           | sequence | postgres |
+| public | team                   | table    | postgres |
+| public | users                  | table    | postgres |
--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const Router = require("express-promise-router");
 const db = require("../db");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const saltRounds = 10;
 const router = new Router();
 
@@ -8,29 +9,39 @@ module.exports = router;
 
 // Create new user
 
-router.post("/signup", async (req, res) => {
-  const { email_address, password } = req.body;
-  const password_hash = await bcrypt.hash(password, saltRounds);
+router.post("/register", async (req, res) => {
   try {
-    const resp = await db.query(
-      "INSERT INTO users (email_address, password_hash) VALUES($1,$2) ON CONFLICT (email_address) DO NOTHING RETURNING *;",
-      [email_address, password_hash]
-    );
-    if (!resp.rowCount) {
-      res.status(401).send({
-        message: "User already exists",
-      });
-    } else {
-      res.status(201).send({
-        message: "User Created!",
-        user: {
-          id: resp.rows[0].id,
-          email_address: resp.rows[0].email_address,
-        },
-      });
+    const { email_address, password } = req.body;
+    if (!(email_address && password)) {
+      res.status(400).send({ message: "Missing Email or Password" });
     }
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+    const { rows } = await db.query(
+      "INSERT INTO users(email_address,password_hash) VALUES ($1,$2) ON CONFLICT (email_address) DO NOTHING RETURNING *;",
+      [email_address, hashedPassword]
+    );
+    if (!rows[0]) {
+      return res
+        .status(409)
+        .send({ message: "User Already Exists. Please Login" });
+    }
+    const token = await jwt.sign(
+      { user_id: rows[0].id, email_address: rows[0].email_address },
+      process.env.TOKEN_KEY,
+      {
+        expiresIn: "2h",
+      }
+    );
+    res.status(201).send({
+      message: "User Created",
+      user: {
+        id: rows[0].id,
+        email_address: rows[0].email_address,
+        token: token,
+      },
+    });
   } catch (err) {
-    console.error(err.stack);
+    console.log(err);
   }
 });
 
